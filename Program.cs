ab4ff3cef1d8ebfa84c227803274c52797e01420
Danilo Costa <dandan_costa2222@outlook.com>
2025-05-23 23:58:52 -0300

    codigo do exercicio

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int n, i, produto;
+
+Console.WriteLine("--- Tabuada ---\n");
+
+Console.Write("Digite o numero a ser multiplicado: ");
+n = int.Parse(Console.ReadLine()!);
+
+for (i = 0; i <= 10; i++){
+    produto = n * i;
+    Console.WriteLine($"{n} x {i} = {produto}");
+}
